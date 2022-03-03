7164868f0af6d0a2dbaa96ec30fca7762bfab52e
Christopher Davenport <chris.davenport@banno.com>
2022-03-03 08:35:39 -0800

    Fix Example to remove signals

--- core/src/main/scala/io/chrisdavenport/testembertimoeut/Main.scala
+++ core/src/main/scala/io/chrisdavenport/testembertimoeut/Main.scala
@@ -7,12 +7,20 @@ import org.http4s.ember.client.EmberClientBuilder
 import org.http4s.ember.server.EmberServerBuilder
 import org.http4s.implicits._
 import com.comcast.ip4s._
+import scala.concurrent.duration._
 
 object TestEmberClientHang extends IOApp.Simple {
-  def run: IO[Unit] = EmberClientBuilder.default[IO].build.use(_.statusFromUri(uri"http://localhost:8765")).void
+  def run: IO[Unit] = EmberClientBuilder.default[IO].build.use(client => 
+    IO.realTime.flatMap( start => 
+      IO.race(
+        client.statusFromUri(uri"http://localhost:8765"),
+        IO.sleep(2.seconds)
+      ).flatMap( out => 
+        IO.realTime.flatMap{end => 
+          val took = end - start
+          IO.println(s"Took: $took, $out").void
+        }
+      )
+    )
+  )
 }
-
-// object RunServer extends IOApp.Simple {
-//   def run: IO[Unit] =
-//     EmberServerBuilder.default[IO].withPort(port"8765").withHttpApp(Kleisli(_ => IO.never)).build.useForever
-// }
--- java-example.sh
+++ java-example.sh
@@ -1,3 +1,5 @@
 sbt -error coreJVM/assembly
 
+
+netcat -l -t -p 8765 &
 java -jar core/.jvm/target/scala-2.13/example.jar
